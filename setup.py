bd5e63878d98898c14247c9aadb78223b2fcd7d5
掉漆 <diaoq@reachauto.com>
2025-04-01 10:04:29 +0800

    init

--- setup.py
+++ setup.py
@@ -4,20 +4,27 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="json2",
-    version="0.9.0",
-    author="Adrian Thoenig",
-    description="Add convenience function to pythons default json implementation",
+    name="mindspore-json",
+    version="0.1.0",
+    author="Your Name",
+    description="MindSpore JSON utilities and machine learning models",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ThoenigAdrian/json2",
+    url="https://github.com/dq77/mindspore-json",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Framework :: MindSpore",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
-    python_requires='>=2.5',
+    python_requires='>=3.7',
+    install_requires=[
+        'mindspore>=1.0.0',
+        'numpy>=1.17.0',
+        'easydict>=1.9',
+        'matplotlib>=3.3.0'
+    ],
     setup_requires=['wheel'],
 )
