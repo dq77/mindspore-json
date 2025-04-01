2d19bef1b1918ccf4c996aecb3d79c846139a9aa
掉漆 <diaoq@reachauto.com>
2025-04-01 10:16:05 +0800

    init

--- README.md
+++ README.md
@@ -1,22 +1,14 @@
 # MindSpore-JSON
 
-## 简介
-MindSpore-JSON是一个基于MindSpore深度学习框架的机器学习模型集合，包含了多个常用的机器学习算法实现，如ResNet50、K-means聚类、线性回归、逻辑回归和决策树等。
 
-## 安装
+## install
 
 ```bash
 pip install mindspore-json
 ```
 
-## 功能特点
-- ResNet50图像分类模型
-- K-means聚类算法
-- 线性回归模型
-- 逻辑回归分类器
-- 决策树算法
 
-## 使用示例
+## islist
 ```python
 from mindspore_json import resNet50
 from mindspore_json import kmeans
@@ -24,15 +16,8 @@ from mindspore_json import linear
 from mindspore_json import logistic
 from mindspore_json import tree
 
-# 更多使用示例请参考各模型的文档
 ```
 
-## 依赖要求
-- Python >= 3.7
-- MindSpore >= 1.0.0
-- NumPy >= 1.17.0
-- EasyDict >= 1.9
-- Matplotlib >= 3.3.0
 
 ## Usage
 
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="mindspore-json",
     version="0.1.0",
-    author="Your Name",
+    author="dq77",
     description="MindSpore JSON utilities and machine learning models",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -16,15 +16,9 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Framework :: MindSpore",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
     python_requires='>=3.7',
-    install_requires=[
-        'mindspore>=1.0.0',
-        'numpy>=1.17.0',
-        'easydict>=1.9',
-        'matplotlib>=3.3.0'
-    ],
+    install_requires=[],
     setup_requires=['wheel'],
 )
